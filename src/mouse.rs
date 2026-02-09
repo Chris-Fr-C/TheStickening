99206377caee3f13e0292d32dff13b2c02ef8a64
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2026-02-09 10:38:35 +0100

    slow asfffff

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 /// Mouse movement input structure
 #[derive(Debug)]
 pub struct MouseMovementInput {
@@ -29,13 +31,31 @@ pub fn movement_control(input: &MouseMovementInput) {
         vertical
     };
 
+    // For a big enough frequency the randomness will be smoothed out.
+    // The goal is that since we cant move less than a pixel, when we want to be
+    // slower than a pixel per period, we will just do one every N period, randomly but
+    // generally converging to the expected speed.
     // the Y axis is inverted when sent to screen.
-    let delta_x = (horizontal * sensitivity) as i32;
+    let lucky_num: f32 = rand::random();
+    let mut dxf = horizontal * sensitivity;
+    if dxf.abs() < 1. && dxf.abs() < lucky_num {
+        dxf = 0.;
+    } else if dxf.abs() < 1. && dxf.abs() >= lucky_num {
+        dxf = dxf.signum();
+    }
+    let delta_x = dxf as i32;
     // If you wanna buffer overflow its due to your config.
-    let delta_y = (-vertical * sensitivity) as i32;
+    let mut dyf = -vertical * sensitivity;
+    if dyf.abs() < 1. && dyf.abs() < lucky_num {
+        dyf = 0.;
+    } else if dyf.abs() < 1. && dyf.abs() >= lucky_num {
+        dyf = dyf.signum();
+    }
+    let delta_y = dyf as i32;
+
     print!(
-        "x {} dx {} y {} dy {}  sensitivity {}                \r",
-        horizontal, delta_x, vertical, delta_y, sensitivity
+        "x {} dx {} y {} dy {}  sensitivity {}  lucky_num {}              \r",
+        horizontal, delta_x, vertical, delta_y, sensitivity, lucky_num
     );
     if delta_x != 0 || delta_y != 0 {
         #[cfg(target_os = "windows")]
